09e6e030fbfbc2e24259731e25fa00ece66d34c4
Juan <caicedo1997.futbol@gmail.com>
2021-12-09 19:17:15 -0500

    componente web

--- account_ms/src/main/java/com/calculospromedios/account_ms/exceptions/InsufficientMateriaException.java
+++
@@ -1,7 +0,0 @@
-package com.calculospromedios.account_ms.exceptions;
-
-public class InsufficientMateriaException extends RuntimeException {
-    public InsufficientMateriaException(String message) {
-        super(message);
-    }
-}
--- account_ms/src/main/java/com/calculospromedios/account_ms/repositories/AccountRepository.java
+++
@@ -1,6 +0,0 @@
-package com.calculospromedios.account_ms.repositories;
-
-import com.calculospromedios.account_ms.models.Account;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface AccountRepository extends MongoRepository <Account, String> { }
